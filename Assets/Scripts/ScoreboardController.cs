362cb0924a733667745b2b681dc9bd04349cb1aa
owenhall <owenjameshall@gmail.com>
2014-05-11 02:03:10 +0100

    Scoreboard is in, yay

--- Assets/Scripts/ScoreboardController.cs
+++ Assets/Scripts/ScoreboardController.cs
@@ -2,18 +2,16 @@
 using System.Collections;
 
 public class ScoreboardController : MonoBehaviour {
-	
+
 	int leftScore = 0;
 	int rightScore = 0;
+	int boardWidth = 100;
+	int boardHeight = 50;
 
-	public int GetLeftScore () {
-		return leftScore;
+	void OnGUI () {
+		GUI.Box(new Rect((Screen.width - boardWidth)/2, (3*boardHeight)/2, boardWidth, boardHeight), "Scoreboard\n" + leftScore + " : " + rightScore);
 	}
 
-	public int GetRightScore () {
-		return rightScore;
-	}
-	
 	public void LeftScored () {
 		leftScore++;
 	}
